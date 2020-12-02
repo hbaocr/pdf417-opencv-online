44047b9f201c420062a52ef3d1c01d5c102acc59
hbaocr <hbaocr@hbaocrs-MacBook-Pro.local>
2020-12-03 00:55:45 +0700

    feat/docker support

--- Pdf417Locator/httpService.py
+++ Pdf417Locator/httpService.py
@@ -33,46 +33,63 @@ def parseDecodeResult(results):
     return result
 
 class PDF417LocatorHTTPRequestHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/healthcheck':
+            self.send_response(200)
+            #self.send_header("Set-Cookie", "foo=bar")
+            self.end_headers()
+            self.wfile.write(json.dumps({'status':'OK'}).encode('utf8'))
+        else:
+            self.send_response(200)
+            #self.send_header("Set-Cookie", "foo=bar")
+            self.end_headers()
+            self.wfile.write(json.dumps({'status':'FAIL','detail':'URL not supported'}).encode('utf8'))
+
     def do_POST(self):
-        ret={
-            "data":"",
-            "status":"OK",
-            "details":"OK"
-        }
+        if self.path == '/decodeImg':
+            ret={
+                "data":"",
+                "status":"OK",
+                "details":"OK"
+            }
 
-        try:
-            content_length = int(self.headers['Content-Length']) # <--- Gets the size of data
-            post_data = self.rfile.read(content_length) # <--- Gets the data itself
+            try:
+                content_length = int(self.headers['Content-Length']) # <--- Gets the size of data
+                post_data = self.rfile.read(content_length) # <--- Gets the data itself
 
-            gray=pdf417Locator.loadImgBuffer2Gray(post_data)
-            pdf417_zones=pdf417Locator.locatePDF417(gray)
-            l=len(pdf417_zones)
-            tmpdir=""
-            with tempfile.TemporaryDirectory() as directory:
-                tmpdir=directory
-                for i in range(l):
-                    fpath=os.path.join(directory,str(i)+".jpg")
-                    pdf417Locator.saveBuff2Jpg(pdf417_zones[i],fpath)
-                    #print(fpath)
-                if l>0:
-                    batch_jpg=os.path.join(directory,"*.jpg")
-                    results = reader.decode(batch_jpg)
-                    code=parseDecodeResult(results)
-                    ret["data"]=code
-        except OSError:
-                ret["status"]="FAIL"
-                ret["details"]=str(OSError)
+                gray=pdf417Locator.loadImgBuffer2Gray(post_data)
+                pdf417_zones=pdf417Locator.locatePDF417(gray)
+                l=len(pdf417_zones)
+                tmpdir=""
+                with tempfile.TemporaryDirectory() as directory:
+                    tmpdir=directory
+                    for i in range(l):
+                        fpath=os.path.join(directory,str(i)+".jpg")
+                        pdf417Locator.saveBuff2Jpg(pdf417_zones[i],fpath)
+                        #print(fpath)
+                    if l>0:
+                        batch_jpg=os.path.join(directory,"*.jpg")
+                        results = reader.decode(batch_jpg)
+                        code=parseDecodeResult(results)
+                        ret["data"]=code
+            except OSError:
+                    ret["status"]="FAIL"
+                    ret["details"]=str(OSError)
 
-        #else:
-        #    print("OK")
-     
+            #else:
+            #    print("OK")
+        
 
-        self.send_response(200)
-        self.send_header("Set-Cookie", "foo=bar")
-        self.end_headers()
-        self.wfile.write(json.dumps(ret).encode('utf8'))
+            self.send_response(200)
+            #self.send_header("Set-Cookie", "foo=bar")
+            self.end_headers()
+            self.wfile.write(json.dumps(ret).encode('utf8'))
 
-
+        else:
+            self.send_response(200)
+            #self.send_header("Set-Cookie", "foo=bar")
+            self.end_headers()
+            self.wfile.write(json.dumps({'status':'FAIL','detail':'URL not supported'}).encode('utf8'))
 
 def run(addr='0.0.0.0',port=3000):
     httpd = HTTPServer((addr, port), PDF417LocatorHTTPRequestHandler)
--- Readme.md
+++ Readme.md
@@ -7,3 +7,8 @@ pip3 install --upgrade pip setuptools wheel
 pip3 install numpy opencv-python
 ```
 
+docker run -d -p 80:80 docker/getting-started
+
+https://github.com/hbaocr/python-zxing
+
+
